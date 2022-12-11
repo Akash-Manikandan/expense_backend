df1e6669302b6b235813d3fa8810e90fc984194c
Akash <67492990+Akash-Manikandan@users.noreply.github.com>
2022-12-11 16:32:01 +0530

    Error handling

--- src/expense/expense.service.ts
+++ src/expense/expense.service.ts
@@ -1,63 +1,93 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { addExpenseDto } from './dto/add-expense.dto';
 import * as dayjs from 'dayjs';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class ExpenseService {
   constructor(private readonly prismaService: PrismaService) {}
   async addExpense(expenseData: addExpenseDto) {
     try {
-      const expense = await this.prismaService.expense.create({
-        data: {
-          amount: expenseData.amount,
-          description: expenseData.description,
-          user: {
-            connect: {
-              id: expenseData.userId,
-            },
-          },
-        },
-      });
-      const updateIncome = await this.prismaService.user.update({
-        where: {
-          id: expenseData.userId,
-        },
-        data: {
-          income: {
-            decrement: expenseData.amount,
-          },
-          stats: {
-            connectOrCreate: {
-              where: {
-                userId: expenseData.userId,
-              },
-              create: {
-                day: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-                quota: [0, 0, 0, 0, 0, 0, 0],
+      return await this.prismaService.$transaction(async (tx) => {
+        const expense = await tx.expense.create({
+          data: {
+            amount: expenseData.amount,
+            description: expenseData.description,
+            user: {
+              connect: {
+                id: expenseData.userId,
               },
             },
           },
-        },
-        select: {
-          stats: true,
-          createdAt: true,
-          income: true,
-          id: true,
-        },
+        });
+        const updateIncome = await tx.user.update({
+          where: {
+            id: expenseData.userId,
+          },
+          data: {
+            income: {
+              decrement: expenseData.amount,
+            },
+            stats: {
+              connectOrCreate: {
+                where: {
+                  userId: expenseData.userId,
+                },
+                create: {
+                  day: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+                  quota: [0, 0, 0, 0, 0, 0, 0],
+                },
+              },
+            },
+          },
+          select: {
+            stats: true,
+            createdAt: true,
+            income: true,
+            id: true,
+          },
+        });
+        const dayOfWeek = dayjs(expense.date).day();
+        const stats = updateIncome.stats;
+        stats.quota[dayOfWeek] += expenseData.amount;
+        const users = await tx.stats.update({
+          where: {
+            userId: expenseData.userId,
+          },
+          data: {
+            quota: stats.quota,
+          },
+          select: {
+            user: {
+              select: {
+                id: true,
+                income: true,
+                expense: {
+                  select: {
+                    id: true,
+                    description: true,
+                    amount: true,
+                    date: true,
+                  },
+                },
+              },
+            },
+            id: true,
+            day: true,
+            quota: true,
+          },
+        });
+        return users;
       });
-      const dayOfWeek = dayjs(expense.date).day();
-      const stats = updateIncome.stats;
-      stats.quota[dayOfWeek] += expenseData.amount;
-      await this.prismaService.stats.update({
-        where: {
-          userId: expenseData.userId,
-        },
-        data: {
-          quota: stats.quota,
-        },
-      });
-      return { expense, updateIncome };
-    } catch {}
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2025') {
+          throw new HttpException('User Not Found', HttpStatus.NOT_FOUND);
+        } else {
+          throw error;
+        }
+      }
+    }
   }
 }
