4baeacb42034e809ebe3e0986deff3809e2cd00b
Akash <67492990+Akash-Manikandan@users.noreply.github.com>
2022-12-11 19:05:23 +0530

    stats

--- src/stats/stats.controller.ts
+++ src/stats/stats.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { StatsService } from './stats.service';
 
 @Controller('stats')
-export class StatsController {}
+export class StatsController {
+  constructor(private readonly statsService: StatsService) {}
+  @Get(':id')
+  async getStats(@Param('id') id: string) {
+    return this.statsService.getStats(id);
+  }
+}
--- src/stats/stats.service.ts
+++ src/stats/stats.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class StatsService {}
+export class StatsService {
+  constructor(private readonly prismaService: PrismaService) {}
+  async getStats(id: string) {
+    const data = await this.prismaService.stats.findMany({
+      where: {
+        userId: id,
+      },
+      select: {
+        id: true,
+        day: true,
+        quota: true,
+      },
+    });
+    return data;
+  }
+}
