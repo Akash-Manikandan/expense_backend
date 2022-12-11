05c3725a6eff117415b61b7efbc8a43c7faf5327
Akash <67492990+Akash-Manikandan@users.noreply.github.com>
2022-12-11 13:56:19 +0530

    ok

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { ExpenseModule } from './expense/expense.module';
+import { StatsModule } from './stats/stats.module';
 
 @Module({
-  imports: [PrismaModule, ConfigModule.forRoot({ isGlobal: true }), UserModule, ExpenseModule],
+  imports: [
+    PrismaModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    UserModule,
+    ExpenseModule,
+    StatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
